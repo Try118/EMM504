e7958533868d3034e898df932ed69d1cc92cebd5
Try118 <1021996158@qq.com>
2019-08-16 19:41:21 +0800

    提交人：姚喜 提交内容：1.引入Bootstrap-dialog代替alert()，不过失败了，还是先用着alert()。 2.引入JQueryValidate进行表单验证。

--- src/main/resources/static/client/main.js
+++ src/main/resources/static/client/main.js
@@ -1,18 +1,42 @@
 //客户管理-注册
 $("button#add").off().on("click",function(){
-	$("div#dialog_client").load("client/add.html",function(){
-		
+	$("div#dialog_client").load("client/add.html",function(){		
 		$(function(){
 			$("div#dialog_client").dialog({
 				title:"客户管理-注册",
 				width:750			
 			});
-		});
+		});		
+		//验证数据
 		
 		$(function(){
+			$("form#createForm").validate({
+				rules:{
+					client_id:{
+						required:true
+					},
+					identify_id:{
+						required:true
+					}
+				},
+				messages:{
+					client_id:{
+						required:"客户编号为空"
+					},
+					identify_id:{
+						required:"身份证号为空"
+					}
+				}
+			});
+		});
+		//拦截表单提交
+		$(function(){
 			$("form#createForm").ajaxForm(function(ResultMessage){
-				//alert(ResultMessage.message);
-				
+				alert(ResultMessage.message);
+//				BootstrapDialog.show({
+//					title:'客户管理',
+//					message:'sadada'
+//				});
 				$("div#dialog_client").dialog("close");
 				$("div#dialog_client").dialog("destory");
 				$("div#dialog_client").html("");
