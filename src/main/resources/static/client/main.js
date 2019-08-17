96a5f9c164c87e57c14b0ab83a9b83eecbd0f30e
Try118 <1021996158@qq.com>
2019-08-17 15:36:35 +0800

    提交人：姚喜
    提交内容：去除dialog的destory方法

--- src/main/resources/static/client/main.js
+++ src/main/resources/static/client/main.js
@@ -1,6 +1,5 @@
-//取得列表数据
 $(function(){	
-	var clientId=null;	
+	var clientId=null;		
 	$("table#ClientGrid").jqGrid({
 		url: 'http://127.0.0.1:8080/client/getListByAllWithPage',
 		datatype: "json",
@@ -28,8 +27,13 @@ $(function(){
 			//alert(clientId);
 		}
 	});
-	
-	
+
+	function reloadList()
+	{
+		$("table#ClientGrid").jqGrid('setGridParam',{postData:{client_id:client_id,identify_id:identify_ide}}).trigger("reloadGrid");		
+	}
+
+
 	//客户管理-删除
 	$("button#delete").off().on("click",function(){
 		if(clientId==null){
@@ -42,7 +46,7 @@ $(function(){
 			});
 		}
 	});
-	
+
 	//客户管理-修改
 	$("button#update").off().on("click",function(){
 		if(clientId==null){
@@ -68,8 +72,9 @@ $(function(){
 									alert("修改客户失败");
 								}
 								$("div#dialog_client").dialog("close");
-								$("div#dialog_client").dialog("destory");
+								//$("div#dialog_client").dialog("destory");
 								$("div#dialog_client").html("");
+								//reloadList();
 							});
 						});
 					});					
@@ -77,55 +82,56 @@ $(function(){
 			});
 		}
 	});
-});
-
-
-//客户管理-注册
-$("button#add").off().on("click",function(){
-	$("div#dialog_client").load("client/add.html",function(){		
-		$(function(){
-			$("div#dialog_client").dialog({
-				title:"客户管理-注册",
-				width:750			
-			});
-		});		
-		//验证数据
-		
-		$(function(){
-			$("form#createForm").validate({
-				rules:{
-					client_id:{
-						required:true
+	//客户管理-注册
+	$("button#add").off().on("click",function(){
+		$("div#dialog_client").load("client/add.html",function(){		
+			$(function(){
+				$("div#dialog_client").dialog({
+					title:"客户管理-注册",
+					width:750			
+				});
+			});		
+			//验证数据
+			
+			$(function(){
+				$("form#createForm").validate({
+					rules:{
+						client_id:{
+							required:true
+						},
+						identify_id:{
+							required:true
+						}
 					},
-					identify_id:{
-						required:true
+					messages:{
+						client_id:{
+							required:"客户编号为空"
+						},
+						identify_id:{
+							required:"身份证号为空"
+						}
 					}
-				},
-				messages:{
-					client_id:{
-						required:"客户编号为空"
-					},
-					identify_id:{
-						required:"身份证号为空"
-					}
-				}
+				});
 			});
-		});
-		//拦截表单提交
-		$(function(){
-			$("form#createForm").ajaxForm(function(ResultMessage){
-				alert(ResultMessage.message);
-//				BootstrapDialog.show({
-//					title:'客户管理',
-//					message:'sadada'
-//				});
-				$("div#dialog_client").dialog("close");
-				$("div#dialog_client").dialog("destory");
-				$("div#dialog_client").html("");
+			//拦截表单提交
+			$(function(){
+				$("form#createForm").ajaxForm(function(ResultMessage){
+					alert(ResultMessage.message);
+	//				BootstrapDialog.show({
+	//					title:'客户管理',
+	//					message:'sadada'
+	//				});
+					$("div#dialog_client").dialog("close");
+					//$("div#dialog_client").dialog("destory");
+					$("div#dialog_client").html("");
+				});
 			});
 		});
 	});
 });
+
+
+
 
 
 
