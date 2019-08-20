acecc4eb40975b2500e7b09294e46be64582ad07
e <1195320026@qq.com>
2019-08-20 23:41:18 +0800

    OutPartsMapper OutPartsMapper.xml

---
+++ src/main/java/com/yx/emm504/outParts/mapper/IOutPartsMapper.java
@@ -0,0 +1,19 @@
+package com.yx.emm504.outParts.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.yx.emm504.Model.OutPartsModel;
+
+@Mapper
+public interface IOutPartsMapper {
+
+	public List<OutPartsModel> selectListByAll() throws Exception;
+	
+	public void create(OutPartsModel outPartsModel) throws Exception;
+	
+	public void delete(OutPartsModel outPartsModel) throws Exception;
+	
+	public void modify(OutPartsModel outPartsModel) throws Exception;
+}
---
+++ src/main/java/com/yx/emm504/outParts/service/IOutPartsService.java
@@ -0,0 +1,12 @@
+package com.yx.emm504.outParts.service;
+
+import java.util.List;
+
+import com.yx.emm504.Model.OutPartsModel;
+
+public interface IOutPartsService {
+
+	public List<OutPartsModel> getListByAll() throws Exception;
+	
+	
+}
