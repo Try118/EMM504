e17ebbd2ac2b4ffe183ad12b02428e763ecc80d5
AChen <AChen@DESKTOP-ROJS9E9>
2019-08-20 19:58:41 +0800

    添加维修模块Mapper接口，xml,model类--chen

---
+++ src/main/java/com/yx/emm504/Model/maintain/MaintainInfo.java
@@ -0,0 +1,85 @@
+package com.yx.emm504.Model.maintain;
+
+import java.util.Date;
+
+public class MaintainInfo {
+    private String productId;
+
+    private String maintainStatus;
+
+    private String maintainNote;
+
+    private String checkNote;
+
+    private String maintainMan;
+
+    private Date processDate;
+
+    private String usedParts;
+
+    private Long workload;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId == null ? null : productId.trim();
+    }
+
+    public String getMaintainStatus() {
+        return maintainStatus;
+    }
+
+    public void setMaintainStatus(String maintainStatus) {
+        this.maintainStatus = maintainStatus == null ? null : maintainStatus.trim();
+    }
+
+    public String getMaintainNote() {
+        return maintainNote;
+    }
+
+    public void setMaintainNote(String maintainNote) {
+        this.maintainNote = maintainNote == null ? null : maintainNote.trim();
+    }
+
+    public String getCheckNote() {
+        return checkNote;
+    }
+
+    public void setCheckNote(String checkNote) {
+        this.checkNote = checkNote == null ? null : checkNote.trim();
+    }
+
+    public String getMaintainMan() {
+        return maintainMan;
+    }
+
+    public void setMaintainMan(String maintainMan) {
+        this.maintainMan = maintainMan == null ? null : maintainMan.trim();
+    }
+
+    public Date getProcessDate() {
+        return processDate;
+    }
+
+    public void setProcessDate(Date processDate) {
+        this.processDate = processDate;
+    }
+
+    public String getUsedParts() {
+        return usedParts;
+    }
+
+    public void setUsedParts(String usedParts) {
+        this.usedParts = usedParts == null ? null : usedParts.trim();
+    }
+
+    public Long getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(Long workload) {
+        this.workload = workload;
+    }
+}
