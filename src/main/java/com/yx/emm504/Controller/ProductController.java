f613f8f7c46d8436ca4232af1151fb1bb3fc4102
ljtlcj <1521826348@qq.com>
2019-08-21 08:39:27 +0800

    "lcj"

--- src/main/java/com/yx/emm504/Controller/ProductController.java
+++ src/main/java/com/yx/emm504/Controller/ProductController.java
@@ -37,6 +37,7 @@ public class ProductController {
 
     @DeleteMapping("/news")
     public ResultMessage<ProductInfo> DeleteProductNew(ProductInfo productInfo){
+
         Boolean aBoolean = productService.DeleteProductNew(productInfo);
         if (aBoolean) return new ResultMessage<ProductInfo>("OK","删除成功");
         return new ResultMessage<ProductInfo>("Error","删除失败");
